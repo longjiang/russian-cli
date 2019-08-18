59faf486c866441fa4b113d15f65ed3cc6fc1c25
Jon Long <jon.long@voxelcreative.ca>
2019-08-18 06:55:54 -0400

    Fix lookup endings

--- src/views/Endings.vue
+++ src/views/Endings.vue
@@ -8,64 +8,10 @@
       </div>
       <div class="row">
         <div class="col-md-4" role="form">
-          <div class="input-group mb-3">
-            <input
-              type="text"
-              id="search-field-1"
-              class="form-control"
-              placeholder="ых"
-              v-model="ending1"
-            />
-            <div class="input-group-append">
-              <button id="lookup-1" class="btn btn-primary" type="botton">
-                <font-awesome-icon icon="search" />
-              </button>
-            </div>
-          </div>
-          <div v-if="ending1" class="results-1">
-            <p class="result-heading-1">
-              A word ending in
-              <b>"-{{ ending1 }}"</b> can be:
-            </p>
-            <ul class="result-list-1">
-              <li
-                class="result-item"
-                v-for="(message, index) in messages1"
-                v-html="message"
-                :key="`m1-${mKey + index}`"
-              ></li>
-            </ul>
-          </div>
+          <EndingLookup v-if="files" :files="files" />
         </div>
         <div class="col-md-4" role="form">
-          <div class="input-group mb-3">
-            <input
-              type="text"
-              id="search-field-2"
-              class="form-control"
-              placeholder="ках"
-              v-model="ending2"
-            />
-            <div class="input-group-append">
-              <button id="lookup-2" class="btn btn-primary" type="botton">
-                <font-awesome-icon icon="search" />
-              </button>
-            </div>
-          </div>
-          <div v-if="ending2" class="results-2">
-            <p class="result-heading-2">
-              A word ending in
-              <b>"-{{ ending2 }}"</b> can be:
-            </p>
-            <ul class="result-list-2">
-              <li
-                class="result-item"
-                v-for="(message, index) in messages2"
-                v-html="message"
-                :key="`m1-${mKey + index}`"
-              ></li>
-            </ul>
-          </div>
+          <EndingLookup v-if="files" :files="files" />
         </div>
         <div class="col-md-4" role="form">
           <div class="input-group mb-3">
@@ -103,31 +49,25 @@
 </template>
 
 <script>
+import EndingLookup from '@/components/EndingLookup'
+
 export default {
+  components: {
+    EndingLookup
+  },
   mounted() {
     loadAllCsvsThen(files => {
       this.files = files
-      // attachIndexEventListeners()
     })
   },
   data() {
     return {
-      ending1: '',
-      ending2: '',
       files: undefined,
-      preposition: '',
-      mKey: 0
+      mKey: 0,
+      preposition: ''
     }
   },
   computed: {
-    messages1() {
-      // return [this.ending1]
-      return lookup(this.ending1, this.files)
-    },
-    messages2() {
-      // return [this.ending2]
-      return lookup(this.ending2, this.files)
-    },
     prepositionMessages() {
       // return [this.ending2]
       return lookupPrepositions(this.preposition, this.files)
