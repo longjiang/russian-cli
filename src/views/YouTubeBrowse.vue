b2bf2dd3acb5666ee48acca80c4a389f028c46e3
Jon Long <jon.long@voxelcreative.ca>
2019-09-04 15:27:19 -0700

    Add sticky class and add Leningrad as one of the recommended channels.

--- src/views/YouTubeBrowse.vue
+++ src/views/YouTubeBrowse.vue
@@ -88,6 +88,13 @@ export default {
             'A channel with lots of videos with Russian and English subtitles.',
           avatar:
             'https://yt3.ggpht.com/a/AGF-l78DSGaofeLyN2aTHkcmcMYZFSZQjyjZwUNdRA=s288-c-k-c0xffffffff-no-rj-mo'
+        },
+        {
+          id: 'UCY0C6A3t3RTUN3BB65rWAgQ',
+          title: 'Ленинград | Leningrad',
+          description: 'A music group from St. Petersburg.',
+          avatar:
+            'https://yt3.ggpht.com/a-/AAuE7mAmbx8EQTBRJDRtTjBhUhhJhZoImQ3JAoLpOjA4Tw=s48-c-k-c0xffffffff-no-nd-rj'
         }
       ]
     }
